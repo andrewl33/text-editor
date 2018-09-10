9e571a9ac3812481f6858bc1a635bf4ad8c325bc
Andrew Lee <and.rob.lee@gmail.com>
2018-09-10 15:34:21 -0700

    Remove sample TypeORM project

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,27 +1,4 @@
-// import {UserController} from "./controller/UserController";
 import {CodeController} from "./controller/CodeController";
-
-// export const Routes = [{
-//     method: "get",
-//     route: "/users",
-//     controller: UserController,
-//     action: "all"
-// }, {
-//     method: "get",
-//     route: "/users/:id",
-//     controller: UserController,
-//     action: "one"
-// }, {
-//     method: "post",
-//     route: "/users",
-//     controller: UserController,
-//     action: "save"
-// }, {
-//     method: "delete",
-//     route: "/users",
-//     controller: UserController,
-//     action: "remove"
-// }];
 
 export const Routes = [{
     method: "get",
